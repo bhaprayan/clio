da6c60904394433065a4458c0ae4d92633fb3c18
Shuby Deshpande <bhaprayan@gmail.com>
2020-04-12 10:02:29 -0400

    cleaned up options page

--- options.js
+++ options.js
@@ -2,20 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
+"use strict";
 
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+const options = [];
 
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
+function constructOptions(options) {
+  return;
 }
-constructOptions(kButtonColors);
+constructOptions(options);
